343befe2cbc6b9fb8086b99c02d0f66a3d82f861
Phillip Zeratul <p.f.zeratul@gmail.com>
2020-11-30 22:56:22 +0800

    Building up

---
+++ Assets/Scripts/Raymarcher.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class Raymarcher : MonoBehaviour
+{
+    public Shader shader;
+
+    private Camera mainCamera;
+    private Material material;
+
+    private Vector3[] frustumCorners = new Vector3[4];
+    private Vector4[] screenTriangleCorners = new Vector4[3];
+
+    private int screenTriangleCornersId = Shader.PropertyToID("_ScreenTriangleCorners");
+
+    private void Start()
+    {
+        mainCamera = GetComponent<Camera>();
+        material = new Material(shader);
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(material);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        mainCamera.CalculateFrustumCorners(new Rect(0, 0, 1, 1), mainCamera.farClipPlane, mainCamera.stereoActiveEye, frustumCorners);
+
+        screenTriangleCorners[0] = new Vector4(frustumCorners[1].x, frustumCorners[1].y, mainCamera.farClipPlane, 0);
+        screenTriangleCorners[1] = new Vector4(frustumCorners[0].x, -3.0f * frustumCorners[1].y, mainCamera.farClipPlane, 0);
+        screenTriangleCorners[2] = new Vector4(3.0f * frustumCorners[2].x, frustumCorners[1].y, mainCamera.farClipPlane, 0);
+
+        screenTriangleCorners[0] = mainCamera.transform.TransformVector(screenTriangleCorners[0]) / mainCamera.farClipPlane;
+        screenTriangleCorners[1] = mainCamera.transform.TransformVector(screenTriangleCorners[1]) / mainCamera.farClipPlane;
+        screenTriangleCorners[2] = mainCamera.transform.TransformVector(screenTriangleCorners[2]) / mainCamera.farClipPlane;
+
+        material.SetVectorArray(screenTriangleCornersId, screenTriangleCorners);
+
+        Graphics.Blit(source, destination, material);
+    }
+}
---
+++ Assets/Shaders/Raymarcher.shader
@@ -0,0 +1,66 @@
+﻿Shader "Zeratul/Raymarcher"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+
+    SubShader
+    {
+        Cull Off
+        ZWrite Off
+        ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                uint id : SV_VertexID;
+            };
+
+            struct v2f
+            {
+                float4 position : SV_POSITION;
+                float2 uv : TEXCOORD0;
+                float3 viewDir : TEXCOORD1;
+            };
+
+            SamplerState sampler_bilinear_clamp;
+            Texture2D<float4> _MainTex;
+            float4 _ScreenTriangleCorners[3];
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                UNITY_INITIALIZE_OUTPUT(v2f, o);
+
+                o.uv.x = (v.id == 2) ? 2.0 : 0.0;
+                o.uv.y = (v.id == 1) ? 2.0 : 0.0;
+
+                o.position = float4(o.uv * float2(2.0, -2.0) + float2(-1.0, 1.0), 1.0, 1.0);
+
+                #if UNITY_UV_STARTS_AT_TOP
+                    o.uv = o.uv * float2(1.0, -1.0) + float2(0.0, 1.0);
+                #endif
+
+                o.viewDir = _ScreenTriangleCorners[v.id].xyz;
+
+                return o;
+            }
+
+            float4 frag (v2f IN) : SV_Target
+            {
+                float3 viewDirWS = IN.viewDir;
+
+                return float4(viewDirWS, 0);
+            }
+            ENDCG
+        }
+    }
+}
